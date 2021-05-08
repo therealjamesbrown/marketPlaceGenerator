6a34c5263dc13c3d2d35f3ba16cac93e9986fa2c
James Brown <james@Jamess-iMac.local>
2021-05-08 13:08:43 -0500

    updating

---
+++ server/routes/root/crud-api.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * ================================
+ * ; Title: MarketPlaceGenerator
+ * ; Authors: James Brown
+ * ; Date: 05/08/2021
+ * ; Description: models for root business cases
+ * ================================
+ * 
+ */
+
+
