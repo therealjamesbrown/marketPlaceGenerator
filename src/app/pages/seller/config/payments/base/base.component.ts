a64d0fd12d3b84a1346dbc10bc4264b077be8f7d
therealjamesbrown <jamesbbb1@gmail.com>
2021-08-19 15:39:41 -0700

    imported paypal

--- src/app/pages/seller/config/payments/base/base.component.ts
+++ src/app/pages/seller/config/payments/base/base.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { PaymentsService } from 'src/app/pages/administration/services/payments.service';
 import { PaypalCommerceComponent } from '../paypal-commerce/paypal-commerce.component';
@@ -41,7 +41,96 @@ export class BaseComponent implements OnInit {
   
    }
 
-  ngOnInit(): void {  }
+   @ViewChild('paypalRef', {static: true}) private paypalRef: ElementRef;
+  ngOnInit(): void {
+    console.log(window.paypal)
+    window.paypal
+    .Buttons({
+      style: {
+        layout: 'horizontal'
+      },
+
+      createOrder: (data, actions) => {
+        return actions.order.create({
+          intent: "CAPTURE",
+          purchase_units: [
+            {
+              amount: {
+                currency_code:"USD",
+                            value:17.00,
+                            breakdown:{
+                                item_total:{
+                                    value: 17.00,
+                                    currency_code: "USD"
+                                }
+                            }
+              },
+            shipping: {
+                address: {
+                  address_line_1: "2211 N First Street",
+                  address_line_2: "Building 17",
+                  admin_area_2: "San Jose",
+                  admin_area_1: "CA",
+                  postal_code: "95131",
+                  country_code: "US"
+                }
+              },
+          items:[{
+                name:"Hamburger",
+            description:"No cheese, add mayo, mustard, pickles",
+                quantity:1,
+                unit_amount:{
+              value:"9.00",
+              currency_code:"USD"
+              }
+                },
+            {
+                name:"Hot Dog",
+            description:"Add relish, onion, ketchup, peppers",
+                quantity:1,
+                unit_amount:{
+              value:"6.00",
+              currency_code:"USD"
+              }
+                },
+            {
+            name:"20 Oz Soda",
+            description:"Dr Pepper",
+                quantity:1,
+                unit_amount:{
+              value:"2.00",
+              currency_code:"USD"
+              }
+                }
+            ]
+            }
+            ],
+            application_context: {
+              /*Uncomment the below app context to disable the prompt for shipping*/
+            //shipping_preference:"NO_SHIPPING",
+            logo_image:"https://i.imgur.com/JOO1P6I.jpeg",
+            //return url won't have any impact, but just including it... You'd use actions.redirect() in onApprove instead.
+            return_url: "https://www.example.com/success",
+            user_action: "PAY_NOW"
+            }
+        })
+      },
+      // Finalize the transaction after payer approval
+      onApprove: function(data, actions) {
+      
+        actions.order.capture().then(function(details) {
+          console.log(details)
+        })
+      },
+      onError: function(data, actions){
+        console.log('something went wrong.')
+      }
+    }).render(this.paypalRef.nativeElement)
+    }
+
+
+
+
 
   configurePaymentSource(element){
     //configure paypal commerce platform
